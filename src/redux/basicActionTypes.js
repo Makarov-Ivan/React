02dd6d357a2f0d834ea692f66bd8cbc17dd46978
Makarov-Ivan <makivanik@gmail.com>
2020-09-03 01:39:47 +0300

    create basi action types

---
+++ src/redux/basicActionTypes.js
@@ -0,0 +1,4 @@
+export const searchStart = 'SEARCH_START';
+export const searchSuccess = 'SEARCH_SUCCESS';
+export const searchFailure = 'SEARCH_FAILURE';
+export const getDataSuccess = 'GET_DATA_SUCCESS';
--- src/redux/playlist/actionTypes.js
+++
@@ -1,4 +0,0 @@
-export const searchStart = 'SEARCH_START';
-export const searchSuccess = 'SEARCH_SUCCESS';
-export const searchFailure = 'SEARCH_FAILURE';
-export const getDataSuccess = 'GET_DATA_SUCCESS';
