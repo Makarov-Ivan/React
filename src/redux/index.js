4a1e0378bbc2f7cd5b7e96fa2cdb5a6b27af0b0b
Makarov-Ivan <makivanik@gmail.com>
2020-09-03 02:19:13 +0300

    delete useless newlines

--- src/redux/index.js
+++ src/redux/index.js
@@ -1,18 +1,8 @@
-import {
-  combineReducers
-} from "redux";
-import {
-  albumReducer
-} from "./album/reducer";
-import {
-  artistReducer
-} from "./artist/reducer";
-import {
-  playlistReducer
-} from "./playlist/reducer";
-import {
-  trackReducer
-} from "./track/reducer";
+import { combineReducers } from "redux";
+import { albumReducer } from "./album/reducer";
+import { artistReducer } from "./artist/reducer";
+import { playlistReducer } from "./playlist/reducer";
+import { trackReducer } from "./track/reducer";
 
 export const allReducers = combineReducers({
   album: albumReducer,
