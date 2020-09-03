43adea6530794e14f473b774d4328f89d24cbda3
Makarov-Ivan <makivanik@gmail.com>
2020-09-03 15:37:51 +0300

    minor changes in App.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect } from 'react';
 import './App.css';
-
 import * as API from "./API/api";
-
 import { Login } from "./components/logIn";
 import { IsLoggedIn } from "./components/isLoggedIn";
 import { LogoComponent } from "./components/logo";
 import { useSelector, useDispatch } from 'react-redux';
 import { setTokens, setKey } from './redux/token/actionTypes'
 import { tokenActionCreator } from './redux/token/actionCreater'
+
 function App() {
   const initialKey = useSelector(store => store.token.initial_key)
   const accessToken = useSelector(store => store.token.access_token)
@@ -29,23 +28,25 @@ function App() {
     }
   }, [initialKey])
 
-  if (accessToken) {
-    return (
-      <React.Fragment>
-        <LogoComponent />
-        <IsLoggedIn />
-        <p>acess token</p>
-        {accessToken}
-      </React.Fragment>
-    )
-  } else {
-    return (
-      <React.Fragment>
-        <LogoComponent />
-        <Login />
-      </React.Fragment>
-    )
-  }
+  return (
+    <React.Fragment>
+      <LogoComponent />
+      {accessToken ?
+        (
+          <React.Fragment >
+            <IsLoggedIn />
+            <p>acess token</p>
+            {accessToken}
+          </React.Fragment >
+        ) : (
+          <React.Fragment>
+            <Login />
+          </React.Fragment>
+        )}
+    </React.Fragment>
+
+
+  )
 }
 
 export default App;
